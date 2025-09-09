eec0ced33b1fe701a5891ae9396244dcba8f3ccd
Bruce B. Anderson <anderson.bruce.b@gmail.com>
2025-09-09 03:07:53 -0400

    Initial commit

---
+++ do-toggle.js
@@ -0,0 +1,4 @@
+// @ts-check
+import { BE } from 'be-enhanced/BE.js';
+import { propInfo, resolved, rejected } from 'be-enhanced/cc.js';
+import { dispatchEvent as de } from 'trans-render/positractions/dispatchEvent.js';
